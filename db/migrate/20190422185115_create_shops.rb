29ebd2a073780466cb35eb6866c5b627480ac6ee
Cody Swartz <ctsstc@gmail.com>
2019-04-22 11:51:45 -0700

    Generate resource: shops

--- db/migrate/20190422185115_create_shops.rb
+++ db/migrate/20190422185115_create_shops.rb
@@ -1,6 +1,9 @@
 class CreateShops < ActiveRecord::Migration[5.2]
   def change
     create_table :shops do |t|
+      t.string :name
+      t.string :address
+      t.integer :type_id
 
       t.timestamps
     end
