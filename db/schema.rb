2123a7fb185531735e08a405a18ef5fd2e8b7446
Cody Swartz <ctsstc@gmail.com>
2019-04-22 12:01:19 -0700

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_184413) do
+ActiveRecord::Schema.define(version: 2019_04_22_185609) do
 
   create_table "shells", force: :cascade do |t|
+    t.integer "shell_type"
+    t.boolean "soft"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "type_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
