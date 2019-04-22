82993e281a3bb7540050b6f6488ab664ae08257c
Cody Swartz <ctsstc@gmail.com>
2019-04-22 11:46:03 -0700

    Regenerate taco resource Force it; I'm a real taco now

--- app/models/taco.rb
+++ app/models/taco.rb
@@ -1,13 +1,2 @@
-# frozen_string_literal: true
-
 class Taco < ApplicationRecord
-  attr_accessor :name, :description, :shell, :shop, :ingredients
-
-  def initialize(params = {})
-    @name = params[:name]
-    @description = params[:description]
-    @shell = params[:shell]
-    @shop = params[:shop]
-    @ingredients = params.fetch(:ingredients, [])
-  end
 end
