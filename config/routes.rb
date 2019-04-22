6ad16a11bd1283853e393473f2451ea7678db15c
Cody Swartz <ctsstc@gmail.com>
2019-04-22 11:56:51 -0700

    Generate resource: shell

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :shells
   resources :shops
   resources :tacos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
