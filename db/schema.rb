3b74ed3c7512db1ede8930d701a6039353691417
Cody Swartz <ctsstc@gmail.com>
2019-04-24 11:39:13 -0700

    Forgot to run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_191629) do
+ActiveRecord::Schema.define(version: 2019_04_24_183618) do
 
   create_table "shells", force: :cascade do |t|
-    t.integer "base"
+    t.integer "primary_ingredient"
     t.boolean "soft"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
