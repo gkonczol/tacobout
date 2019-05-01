0c587556d6555677c11d0cf5fcd2de32527a303d
Cody Swartz <ctsstc@gmail.com>
2019-05-01 10:55:04 -0700

    Rails'afy the shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,31 +1,8 @@
 class Shop < ApplicationRecord
-  attr_accessor :is_truck, :location
-  enum primary_ingredient: { truck: 0, restaurant: 1, drive_through: 2 }
-
-  TACOS = {
-    beef_taco: 'nice beef taco with beans',
-    veggie_taco: 'taco with guac and tempe'
+  has_many :taco
+  enum type: {
+    truck: 0,
+    restaurant: 1,
+    drive_through: 2
   }
-
-  def initialize(is_truck, location, rating)
-    @is_truck = is_truck
-    @location = location
-    @rating = rating
-  end
-
-  def get_tacos
-    TACOS
-  end
-
-  def get_location
-    location
-  end
-
-  def rate_up(rating)
-    rating.rate_up(rating) 
-  end
-
-  def get_rating
-    rating.get_rating
-  end
 end
