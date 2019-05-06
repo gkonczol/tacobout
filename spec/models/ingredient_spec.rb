fe39ae7226251efee502273eb08bf4251fa41131
Cody Swartz <ctsstc@gmail.com>
2019-05-06 09:38:40 -0700

    Add ingredient specs Mainly testing rails right now I feel like, but this gets a base in with expectations

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:chicken) { Ingredient.create(name: 'Chicken', spice: 0) }
+  let(:jalapeno) { Ingredient.create(name: 'Jalapeno', spice: 3) }
+  let(:habanero) { Ingredient.create(name: 'Habanero', spice: 5) }
+
+  it 'has a name' do
+    expect(chicken.name).to eq('Chicken')
+  end
+
+  it 'has no spice' do
+    expect(chicken.spice).to eq(0)
+  end
+
+  it 'has medium spice' do
+    expect(jalapeno.spice).to eq(3)
+  end
+
+  it 'has maximum spice' do
+    expect(habanero.spice).to eq(5)
+  end
 end
