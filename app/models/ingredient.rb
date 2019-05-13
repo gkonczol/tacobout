eaedae11c45a6c2772cb2275713560231b6f9086
Cody Swartz <ctsstc@gmail.com>
2019-05-13 11:47:26 -0700

    Clean up old ingredient methods Defaults are now in the database

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,16 +1,3 @@
 class Ingredient < ApplicationRecord
   has_and_belongs_to_many :tacos
-  DEFAULT_SPICE = 0
-  DEFAULT_VEGAN = true
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.spice ||= DEFAULT_SPICE
-    self.vegan ||= DEFAULT_VEGAN
-  end
-  
-  def is_vegan?
-    return DEFAULT_VEGAN
-  end
 end
