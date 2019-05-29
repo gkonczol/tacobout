89e5b434152d8aa4c8cbfd26af997cb0acab94f6
moehnked <moehnked@oregonstate.edu>
2019-05-29 12:36:53 -0700

    fixed gem conflicts and configured rack tests

---
+++ spec/system/add_taco_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'adding a taco', type: :system do
+  it 'allows a user to create a cheesy chicken taco with a soft shell' do
+    visit new_taco_path
+    fill_in 'Name', with: 'Cheesy Chicken Taco'
+    fill_in 'Shell', with: 'Soft'
+    click_on('Bring Taco Into This World')
+    visit tacos_path
+    expect(page).to have_content('Cheesy Chicken Taco')
+    expect(page).to have_content('Soft')
+  end
+end
