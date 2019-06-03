1d87b96ca8fec14d966e94f4ed0ee0ac63d52e8b
gkonczol <konczolg@oregonstate.edu>
2019-06-02 18:54:54 -0700

    Add new tacos incorrectly.
    References #4

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,2 +1,27 @@
 class TacosController < ApplicationController
+  def new
+    @taco = Taco.new
+  end
+
+  def index
+    @tacos = Taco.all
+  end
+
+  def create
+    shell = Shell.create(
+      soft: params[:taco][:shell]
+    )
+    shell.save
+    taco = Taco.create(
+      name: params[:taco][:name], 
+      shell_id: shell.id
+      )
+      taco.save
+      redirect_to tacos_path
+    # if taco.save
+    #   redirect_to tacos_path
+    # else
+    #   render :new
+    # end
+  end
 end
--- spec/system/add_taco_spec.rb
+++ spec/system/add_taco_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'adding a taco', type: :system do
   it 'allows a user to create a cheesy chicken taco with a soft shell' do
     visit new_taco_path
     fill_in 'Name', with: 'Cheesy Chicken Taco'
-    fill_in 'Shell', with: 'Soft'
-    click_on('Bring Taco Into This World')
+    check('Shell')
+    click_on("Create Taco")
     visit tacos_path
     expect(page).to have_content('Cheesy Chicken Taco')
     expect(page).to have_content('Soft')
