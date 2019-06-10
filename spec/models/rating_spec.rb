20d20f86e76f952393ed355d716bb516f334cf58
gkonczol <konczolg@oregonstate.edu>
2019-06-09 18:57:42 -0700

    Move shop rating test from rating spec to taco spec.
    References #10

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,30 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  context 'rating in relation to taco shop' do
-	cheesy = Taco.new
-	meaty = Taco.new
-	cheesy.setup
-	meaty.setup
-	cheesy.rate(3.5)
-	meaty.rate(4.5)
-	subject = Shop.create()
-	subject.setup([cheesy, meaty])
-
-
-    it 'a shop that has not been rated has a rating of zero, regardless of its tacos' do
-      expect(subject.rating).to eq(0)
-    end
-    
-    it 'can be rated, and the first rating entered is the rating of the shop' do
-      subject.rate(5)
-      expect(subject.rating).to eq(5)
-    end
-
-    it 'multiple ratings are averaged to calculate the rating of a shop with more than one rating' do
-      subject.rate(1)
-      expect(subject.rating).to eq(3)
-    end
-
-  end
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -42,4 +42,30 @@ RSpec.describe Shop, type_id: :model do
       expect(subject.type_id).to eq('restaurant')
     end
   end
+
+  context 'rate a taco shop' do
+    cheesy = Taco.new
+    meaty = Taco.new
+    cheesy.setup
+    meaty.setup
+    cheesy.rate(3.5)
+    meaty.rate(4.5)
+    subject = Shop.create()
+    subject.setup([cheesy, meaty])
+  
+  
+      it 'a shop that has not been rated has a rating of zero, regardless of its tacos' do
+        expect(subject.rating).to eq(0)
+      end
+      
+      it 'can be rated, and the first rating entered is the rating of the shop' do
+        subject.rate(5)
+        expect(subject.rating).to eq(5)
+      end
+  
+      it 'multiple ratings are averaged to calculate the rating of a shop with more than one rating' do
+        subject.rate(1)
+        expect(subject.rating).to eq(3)
+      end
+  end
 end
